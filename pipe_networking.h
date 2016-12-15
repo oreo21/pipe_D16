bf06f54359b4529ec27ed677b9b9db7ec0dadfbb
oreo21 <reok99@gmail.com>
2016-12-15 15:33:53 -0500

    added some files!

---
+++ pipe_networking.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define MESSAGE_BUFFER_SIZE 256
+
+int server_handshake(int*);
+int client_handshake(int*);
