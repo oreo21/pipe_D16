bdcc10ae29cdcca0bec091e4f3471deba2805a80
oreo21 <reok99@gmail.com>
2016-12-16 15:34:05 -0500

    worked on stuff

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,14 +2,18 @@
 
 int server_handshake(int* fromClient){
   mkfifo("mario", 0644); //WKP
-
+  printf("server_handshake called\n");
 }
 
 int client_handshake(int* toServer){
-  int fd = open("mario", O_WRONLY);
+  printf("client_handshake called\n");
+  
   char priName[6] = "luigi";
-  mkfifo(priName, 0644); //private FIFO
+  mkfifo(priName, 0644); //private FIFO  
+
+  int fd = open("mario", O_WRONLY);
   write(fd, priName, sizeof(priName));
+  
 }
   
      
